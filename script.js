e5230e34a0b8054588b28dc0ded7eb7397517d2a
irum-shah78 <irams4812@gmail.com>
2024-04-01 16:10:43 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizData = [
+  {
+    question: 'How old is JavaScript Programming Language?',
+    a: '40',
+    b: '30',
+    c: '28',
+    d: '50',
+    correctAnswer: 'c'
+  }, {
+    question: 'Which is most used Programming Language?',
+    a: 'Python',
+    b: 'Java',
+    c: 'C++',
+    d: 'JavaScript',
+    correctAnswer: 'd'
+  }, {
+    question: 'Who is PM of Pakistan?',
+    a: 'Imran Khan',
+    b: 'Shehbaz Sharif',
+    c: 'Asif Ali Zardari',
+    d: 'Nawaz Sharif',
+    correctAnswer: 'b'
+  }, {
+    question: 'HTML stands for?',
+    a: 'Hypertext Markup Language', 
+    b: 'Cascading Style Sheet',
+    c: 'JavaScript Object Notation',
+    d: 'Integrated Developemnt Enviornment',
+    correctAnswer: 'a'
+  }, {
+    question: 'In which JavaScript Launched?',
+    a: '1995',
+    b: '2010',
+    c: '2015',
+    d: 'None of Above',
+    correctAnswer: 'a'
+  }
+]
+
